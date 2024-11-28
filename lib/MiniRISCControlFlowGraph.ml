14fb4819380b751df91eb86fe35d999a25cc55ea
AldoBuzi <aldomob00@gmail.com>
2024-11-28 15:00:19 +0100

    after speaking with professor: we decided to remove unrequired optimization, since they might need proof of safety

--- lib/MiniRISCControlFlowGraph.ml
+++ lib/MiniRISCControlFlowGraph.ml
@@ -56,70 +56,50 @@ let _NONE = "";;
 let no_reg = fun () -> _NONE;;
 let no_peek = no_reg;;
 let rec translate_node node = 
-  let rec translate_ops ops write_to_reg peek_write_reg = 
+  let rec translate_ops ops write_reg = 
     match ops with
     (* Example x := 0 *)
     | MiniImp.Constant(t1) -> 
-        let res = if peek_write_reg() = "" then get_next_register() else write_to_reg() in
-        [LoadI(t1, res)]
+        [LoadI(t1, write_reg ())]
     (* Example y:= 1; x := y *)
     | Variable(t1) -> 
       let t1_reg = get_reg_from_variable t1 in 
-      let res = if peek_write_reg() = "" then get_next_register() else write_to_reg() in
-      [Copy(t1_reg, res)]
+      [Copy(t1_reg, write_reg() )]
     | Plus(t1,t2) | Minus(t1,t2) | Times(t1,t2) -> (match (t1,t2) with
       (* Example x := 1 + 2 *)
       (* This will generate (loadi and addi) or (loadi and multi), etc. *)
       | (Constant(_), Constant(_)) -> 
-          (* second parameter not required *)
           let r1 = get_next_register () in
-          (* optimize registers use by using current temporary reg if variabile is null *)
-          let reg_result = if peek_write_reg() = _NONE then r1 else write_to_reg() in
-          evaluate_ops ops r1 _NONE reg_result
+          evaluate_ops ops r1 _NONE (* second parameter not required *)  (write_reg())
       (* Example y:= 1; x := y + 1 *)
       (* this kind of condition will generate an addi, multi etc. for sure *)
       | (Variable(t1), Constant(_)) | (Constant(_), Variable(t1))  -> 
           let t1_reg = get_reg_from_variable t1 in 
-          let reg_result = if peek_write_reg() = _NONE then get_next_register() else write_to_reg() in
-          evaluate_ops ops t1_reg _NONE (*second parameter not used*) reg_result
+          evaluate_ops ops t1_reg _NONE (*second parameter not used*) (write_reg())
       (* Example y:= 1; z = 0; x := z + y *)
-      (* new register is required for sure, we can't overwrite other variables registers *)
       | (Variable(t1), Variable(t2)) -> 
           let t1_reg = get_reg_from_variable t1 in 
           let t2_reg = get_reg_from_variable t2 in 
-          let reg_result = if peek_write_reg() = _NONE then get_next_register() else write_to_reg() in
-          evaluate_ops ops t1_reg t2_reg reg_result
+          evaluate_ops ops t1_reg t2_reg (write_reg())
       (* If we arrive there, then the assignment it's of the form (x = z + 3 + y * z) *)
-      (* base case: if the variabile doesn't have a reg, then we assign to it the last one we have used  *)
-      (* Not that we can assign it, since for sure the last register is either a new register or a one derived from expressions computation *)
-      (* Therefore the last register is for sure not assigned to another variable *)
       | (Variable(t1), other) | (other, Variable(t1)) ->  
           let t1_reg = get_reg_from_variable t1 in 
           (* this call can generate intermediate registers to allocate temporary results *)
-          let t2_res = translate_ops other no_reg no_peek in 
-          let last = get_last_register () in 
-          (* optimize register usage. we reuse last register if we aren't at then end of evalutation *)
-          let next_register = if peek_write_reg() = _NONE then last else write_to_reg() in  
-          t2_res @ evaluate_ops ops t1_reg last next_register
+          let t2_res = translate_ops other get_next_register in 
+          let last = get_last_register () in     
+          t2_res @ evaluate_ops ops t1_reg last (write_reg())
       (* No variable occur there for sure, otherwise it would have been matched with a previous pattern *)
-      (* we can safeily take the last register *)
-      (* Optimized register use as we compute at most one loadi and then only addi/multi/subi *)
       | (Constant(_), other) | (other, Constant(_)) ->  
         (* this call can generate intermediate registers to allocate temporary results *)
-        let t2_res = translate_ops other no_reg no_peek in 
-        let last = get_last_register () in 
-        let next_register = if peek_write_reg() = _NONE then last else write_to_reg() in  
-        t2_res @ evaluate_ops ops last _NONE next_register
-      (* I can resue the last register *)
-      (* Remember the last register is either a new register or a register allocated by an expression *)
-      (* Either case, the resulting register is for sure not assigned to a variable *)
+        let t2_res = translate_ops other get_next_register in 
+        let last = get_last_register () in   
+        t2_res @ evaluate_ops ops last _NONE (write_reg())            
       | (other, other2) -> 
-        let res1 = translate_ops other no_reg no_peek in 
+        let res1 = translate_ops other get_next_register in 
         let last_reg = get_last_register () in 
-        let res2 = translate_ops other2 no_reg no_peek in 
-        let last_reg2 = get_last_register () in 
-        let next_register = if peek_write_reg() = _NONE then last_reg2 else write_to_reg() in  
-        res1 @ res2 @ evaluate_ops ops last_reg last_reg2 next_register
+        let res2 = translate_ops other2 get_next_register in 
+        let last_reg2 = get_last_register () in         
+        res1 @ res2 @ evaluate_ops ops last_reg last_reg2 (write_reg())
       )
   in
   (* All this operations use additional registers to store intermediate results *)
@@ -130,65 +110,48 @@ let rec translate_node node =
     | Not(t1) -> 
         let res = translate_boolean t1 in 
         let last = get_last_register() in 
-        (* use same register to store the "not" result *)
-        res @ [Not(last,last)]
+        res @ [Not(last,get_next_register())]
     | And(True, t1) | And(t1, True) ->  
         let true_res = translate_boolean t1 in 
-        let last = get_last_register() in  
-        (* use same register to store the result *)
-        true_res @ [AndI(last,1, last)]
+        let last = get_last_register() in        
+        true_res @ [AndI(last,1, get_next_register())]
     | And(False, t1) | And(t1, False) ->  
         let false_res = translate_boolean t1 in 
         let last = get_last_register() in  
-        (* use same register to store the result *)
-        false_res @ [AndI(last, 0, last)]
+        false_res @ [AndI(last, 0, get_next_register())]
     | And(t1,t2) -> 
         let t1_res = translate_boolean t1 in 
         let t1_reg = get_last_register() in 
         let t2_res = translate_boolean t2 in 
         let t2_reg = get_last_register() in 
-        (* use same register to store the result *)
-        t1_res @ t2_res @ [And(t1_reg, t2_reg, t2_reg )]
-    (* get a new register, we can't reuse them since they belong to some variables *)
+        t1_res @ t2_res @ [And(t1_reg, t2_reg, get_next_register() )]
     | Smaller(Variable(t1),Variable(t2)) -> 
       let t1_reg = get_reg_from_variable t1 in 
       let t2_reg = get_reg_from_variable t2  in 
       [Less(t1_reg, t2_reg, get_next_register())]
-    (* for sure t2 is not a variable, otherwise we would have matched the case before *)
-    (* Therefore I can reuse the same register to store the result (hence t2) *)
     | Smaller(Variable(t1),t2) | Smaller(t2,Variable(t1)) -> 
         let t1_reg = get_reg_from_variable t1 in 
-        let t2_res = translate_ops t2 no_reg no_peek in 
+        let t2_res = translate_ops t2 get_next_register in 
         let t2_reg = get_last_register() in   
-        t2_res @[Less(t1_reg, t2_reg, t2_reg)]
-    (* If we fall in this case, then for sure the expression doesn't contain a variable *)
-    (* Therefore I reuse the last register *)
+        t2_res @[Less(t1_reg, t2_reg, get_next_register())]
     | Smaller(t1,t2) -> 
-      let t1_res = translate_ops t1 no_reg no_peek in 
+      let t1_res = translate_ops t1 get_next_register in 
       let t1_reg = get_last_register() in 
-      let t2_res = translate_ops t2 no_reg no_peek in 
+      let t2_res = translate_ops t2 get_next_register in 
       let t2_reg = get_last_register() in   
-      t1_res @ t2_res @[Less(t1_reg, t2_reg, t2_reg)]
+      t1_res @ t2_res @[Less(t1_reg, t2_reg, get_next_register())]
   in
   let translate_statement statement =
     match statement with
     |MiniImpControlFlow.Skip -> [Nop]
     |Assign(variable,t2) -> 
-      (* Assumption #1: If the variable doesn't have a register, then, the first time, we use assign to him the last register used for evaluating its t2  *)
-      (* Therefore, to make sure the assumption holds, we allow throwing an exception if some operation tries to read the variable register without peeking it first *)
       (* Lazy functions to postpone evaluating *)
-      (* translate_ops either uses a register allocated by an expression or a totally new register if the computation involved only variables *)
-      let write_reg () = get_reg_from_variable variable in
+      let write_reg () = get_next_reg_if_none variable in
       let peek_write_reg () = get_reg_or_empty variable in
-      let eval_body = translate_ops t2 write_reg peek_write_reg in
-      (* avoid having different registers for same variables *)
+      let eval_body = translate_ops t2 write_reg in
       (* link variable to the selected register *)
-      (* This assert always holds if intermediate computations haven't broken the assumption *)
-      assert (match (get_reg_or_empty variable, get_last_reg_if_none variable) with
-        |("",_) -> true
-        |(val1,val2) -> val1 = val2 );
-      (link_variable variable (get_last_reg_if_none variable);
-      eval_body (*@ [LoadI(memory_address (),next_register); Store (get_prec_register (), next_register )]*))
+      link_variable variable (get_last_reg_if_none variable);
+      eval_body
     |Guard(cond) -> 
       let translated_cond = translate_boolean cond in 
       translated_cond
